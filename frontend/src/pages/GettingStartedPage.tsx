fba497a3f49d9372fc720c9cb1fe136e759a18d4
Maricris De Jesus <maricris052298dejesus@gmail.com>
2025-07-25 21:23:32 +0800

    refactor: migrate Getting Started page styles to Tailwind CSS

--- frontend/src/pages/GettingStartedPage.tsx
+++ frontend/src/pages/GettingStartedPage.tsx
@@ -1,48 +1,91 @@
 
-import './GettingStarted.css';
-import videoFile from '../assets/Geoff and Jenn Luke.mp4';
-import Thumbnail from '../assets/images/Thumbnail.jpg';
-
+//import './GettingStarted.css';
 function GettingStartedPage() {
 	return (
-		<div className='main-container'>
-			<header>
-				<h1>Thank <span className='you'>You</span></h1>
-				<p className='p1'>for registering!</p>
+		<div className='main-container flex flex-col relative items-center'>
+			<header className="bg-transparent h-fit flex flex-col w-[45%] items-center mb-[15px]">
+				<h1 className="text-[5.5vw] mt-[30px] mb-[10px]">Thank <span className="text-[rgb(189,202,45)]">You</span></h1>
+				<p className="font-[300] text-center">for registering!</p>
 
-				<p className='p2'>You have taken a very important step in building your very own online business!</p>
-				<div className="email-info-container">
-					<p>Check your email/junk mail to see if you got an email from my email address: <span className='email'>info@geoffjen.com</span></p>
+				<p className="mb-[50px] font-[300]">You have taken a very important step in building your very own online business!</p>
+				<div className="email-info-container bg-[rgb(189,202,45)] text-center p-[12px]">
+					<p>Check your email/junk mail to see if you got an email from my email address: <span className='email font-bold'>info@geoffjen.com</span></p>
 				</div>
-				<div className='other-info-container'>
-					<p>I've Emailed a Link to Our <span>Live Webinar</span></p>
+				<div className='other-info-container bg-[rgb(189,202,45)] py-[30px] px-[10px] my-[30px] w-full'>
+					<p className="text-center">I've Emailed a Link to Our <span className="font-bold">Live Webinar</span></p>
 				</div>
 			</header>
-			<section className='webinar-section'>
-				<h1 className='webinar-heading'>
+			<section className="webinar-section bg-white w-full flex items-center flex-col">
+				<h1 className="my-[30px]">
 					Here is a Replay You Can Watch NOW:
 				</h1>
-				<div className='replay-video-wrapper'>
-					<video controls poster={Thumbnail}>
-						<source src={videoFile} type='video/mp4' />
-						Your browser does not support the video tag.
-					</video>
+				<div className='replay-video-wrapper w-[70%] h-[80vh] mb-[50px] bg-red-300 '>
+					  <iframe
+      width="100%"
+      height="100%"
+      src="https://www.youtube.com/embed/qroPTAmlyys"
+      title="Geoff and Jenn Luke"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerPolicy="strict-origin-when-cross-origin"
+      allowFullScreen
+			className=" bg-black"
+    ></iframe> 
 				</div>
-				<button className='join-button'>
+				<button className='join-button 
+				mb-[50px]
+				p-[20px]
+				text-[1.5rem]
+				border-0
+				shadow-[6px_6px_15px]
+				shadow-[rgb(52,52,52)]
+				cursor-pointer
+				text-white
+				bg-[rgb(189,202,45)]
+				active:bg-[rgb(129,138,32)]
+				'>
 					Join Our Success Team Now
 				</button>
 			</section>
 
-			<footer className='footer-section'>
-				<div className='footer-wrapper'>
+			<footer className='footer-section 
+			bg-[#ede9e9]
+			w-full
+			flex
+			justify-center
+			py-[30px]
+			px-[10px]
+			'>
+				<div className='footer-wrapper
+				w-[70%]
+				flex
+				items-center
+				justify-evenly
+				gap-[20px]
+
+				'>
 					<div className='facebook-icon'>
 						<i className="fab fa-square-facebook" style={{ color: "#3297e5ff", fontSize: "5rem" }}></i>
 					</div>
-					<p className='footer-text'>
+					<p className='footer-text
+					text-[1.2rem]
+					flex-2
+					'>
 						Click here to follow me on Facebook so that we can message each other if you have any questions.
 					</p>
 					<a href='https://www.facebook.com/geoff.luke.35' target='_blank'>
-						<button className='add-button'>Add Me On Facebook</button>
+						<button className='add-button
+						text-[1.5rem]
+						flex-1
+						py-[15px]
+						px-[30px]
+						bg-[rgb(189,202,45)]
+						shadow-[6px_6px_10px]
+						shadow-[rgb(82, 82, 82)]
+						border-0
+						text-white
+						cursor-pointer
+						active:bg-[rgb(129,138,32)]
+						'>Add Me On Facebook</button>
 					</a>
 				</div>
 			</footer>
