3e42f2ccef20f981739b4decee207e9b7a1ac5a1
Maricris <maricris052298dejesus@gmail.com>
2025-12-04 12:48:33 +0800

    modified database connection

--- frontend/api/db.js
+++ frontend/api/db.js
@@ -11,10 +11,7 @@ export const connectDB = async () => {
 	}
 
 	try {
-		await mongoose.connect(process.env.MONGO_URI, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
+		await mongoose.connect(process.env.MONGO_URI); // remove deprecated options
 		isConnected = true;
 		console.log("✅ MongoDB connected");
 	} catch (err) {
